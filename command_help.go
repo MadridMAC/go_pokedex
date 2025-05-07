d68ad28634ad125a66b80e51e4e93a3377ac0bf3
MadridMAC <anymordonutzarty@gmail.com>
2025-05-07 12:43:22 +0800

    Added 'exit' and 'help' commands in repl

---
+++ command_help.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for _, command := range commandList() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
--- repl.go
+++ repl.go
@@ -2,10 +2,57 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+func runRepl() {
+	userInput := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		userInput.Scan()
+
+		output := cleanInput(userInput.Text())
+		if len(output) == 0 {
+			continue
+		}
+
+		command := output[0]
+		val, ok := commandList()[command]
+		if ok {
+			val.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+		// fmt.Printf("Your command was: %s\n", command)
+	}
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	split := strings.Fields(lowered)
 	return split
 }
+
+func commandList() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
