8f1fd253167d45bed4cb76e51c6e68bba1b4764b
MadridMAC <anymordonutzarty@gmail.com>
2025-07-31 11:51:52 +0800

    Added simple REPL support

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,23 @@ func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	split := strings.Fields(lowered)
 	return split
+}
+
+func startRepl() {
+	// Create NewScanner to receive Stdin input
+	input := bufio.NewScanner(os.Stdin)
+	// REPL proper; Scan and receive input, then clean it
+	// If no text is received, continue. Otherwise, print the first	cleaned "word" as the command
+	for {
+		fmt.Print("Pokedex > ")
+		input.Scan()
+
+		receivedText := cleanInput(input.Text())
+		if len(receivedText) == 0 {
+			continue
+		}
+		commandText := receivedText[0]
+
+		fmt.Printf("Your command was: %s\n", commandText)
+	}
 }
